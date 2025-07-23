707cb927c5c3b053a61458e33e1f5bb44f21d927
DanielSP99 <daniel.sanchez@ingenierosmafur.com>
2025-07-23 17:44:29 -0600

    Initial Commit

---
+++ ecobici/ecobiciApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecobiciApp.swift
+//  ecobici
+//
+//  Created by Daniel Sanchez Peraza on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ecobiciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
